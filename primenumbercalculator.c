bfc29c1b274a18c74b9d8fc162e7111a42af4db6
davidseitz <seitz.david-it23@it.dhbw-ravensburg.de>
2024-04-29 21:19:08 +0200

    Calculates the primes to 10000

--- primenumbercalculator.c
+++ primenumbercalculator.c
@@ -1,12 +1,40 @@
 #include <stdio.h>
-#include <math.h>
+#include <ctype.h>
+#include <stdint.h>
+uint64_t squarerootapproximation(const uint64_t *number, uint64_t *end)
+{
+//Number has to be bigger than 0
+    if((*end)*(*end)>=*number) {
+        return *end;
+    } else {
+        return ++(*end);
+    }
 
-int main(int argc, char** argv){
-	
+}
+uint64_t primenum(const uint64_t number, uint64_t end)
+{
+    uint64_t i = 2;
+    while( i <= end)
+    {
+        if( number % i==0) {
+            return 0;
+        }
+        ++i;
+    }
+    return number;
+}
+int main(int argc, char** argv) {
 
-	double number = 9;
-	int n = sqrt(&number);
-	printf("Hello World\n");
 
-	return 0;
+    uint64_t number = 1;
+    for (uint64_t i = 1; i<10000; i++) {
+        uint64_t n = squarerootapproximation(&i, &number);
+        if (primenum(i, number) !=0)
+        {
+            printf("Primenum: %li, Squareroot: %li\n",i,number);
+        }
+
+        //printf("Num: %li Aprrox: %li\n", i, n);
+    }
+    return 0;
 }
