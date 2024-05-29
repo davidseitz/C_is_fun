9bafdc098e0a67faf2a0655016e71172fc35a0a6
davidseitz <seitz.david-it23@it.dhbw-ravensburg.de>
2024-05-29 18:46:34 +0200

    Format idea finalised

--- C_git_automatic_script/C_git_automatic_script.c
+++ C_git_automatic_script/C_git_automatic_script.c
@@ -1,12 +1,29 @@
 #include <stdio.h>
+#include <string.h>
 
 int main(int argc, char **argv)
 {
-    if (argc != 2) {
-        printf("Only give one argument\n");
+    if (argc == 1) {
+        printf("Give at least 1 argument\n");
         return 1;
     } else {
-        printf("Your argument: %s\n", argv[1]);
+        printf("The files you'd like to add: %s\n", argv[1]);
+        printf("Your comment: %s\n",argv[2]);
+        printf("Does this look right to you (Y,n)\n");
+        for (int i = 3; i>=0; --i) {
+            char option = fgetc(stdin);
+            if (option == 'Y') {
+                printf("Executing commands\n");
+            } else if (option == 'n') {
+                return 0;
+            } else {
+                printf("Unkown Option: %c\n",option);
+            }
+            if(i!=0)
+            {
+                printf("You got %i more tries left before the Programm auto-terminates\n",i);
+            }
+        }
     }
     return 0;
 }
